f31c4d72e791212a806467ae68a4bf2411cb4a21
Pavel Kunc <pavel.kunc@gmail.com>
2010-02-21 14:59:57 +0000

    [merb-auth-more] Explicitly require merb-auth-core.

--- merb-auth-more/lib/merb-auth-more.rb
+++ merb-auth-more/lib/merb-auth-more.rb
@@ -1,3 +1,5 @@
+require 'merb-auth-core'
+
 # make sure we're running inside Merb
 if defined?(Merb::Plugins)
   # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
